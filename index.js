b16d97f3c60a2d2dad3d7e8dc643b01bffe8bea7
Marjory Savanini <marjory.savanini2@gmail.com>
2024-09-15 15:51:42 +0200

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -46,7 +46,22 @@ const listarMetas = async () => {
 
     console.log("Meta(s) concluída(s)")
 }
-    
+    const metasRealizadas = async () => {
+        const realizadas = metas.filter((meta) =>{
+            return meta.checked
+        })
+
+    if(realizadas.length == 0){
+        console.log("Não existem metas realizadas :(")
+        return
+    }
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+
+    }
+
     const start = async () => {
         while (true){
 
@@ -62,6 +77,10 @@ const listarMetas = async () => {
                         value: "listar"
                     },
                     {
+                        name: "Metas realizadas",
+                        value: "realizadas"
+                    },
+                    {
                         name: "Sair",
                         value: "sair"
                     }
@@ -75,6 +94,9 @@ const listarMetas = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima!")
